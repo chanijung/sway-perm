616b8c6cd83b0c87aeefeac389c0afc8cf265e43
chanijung <1016chani@kaist.ac.kr>
2021-07-15 01:47:52 +0900

    rename

--- evaluate.sh
+++ evaluate.sh
@@ -6,7 +6,8 @@ declare -a arr=("flex" "grep" "gzip" "sed")
 ## loop!
 for i in "${arr[@]}"
 do
-	python3.6 main_linux.py -d "$i"&
+	# python3.6 main_linux.py -d "$i"&
+	python3.6 main_linux.py -d "$i" -init 4 -n 2&
 done
 
 exit 1
